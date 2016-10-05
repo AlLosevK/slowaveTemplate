696b8670839dad3489d5ff78a50dde3dcda0794f
AlLosevK <al.losev.k@gmail.com>
2016-10-06 01:11:32 +0300

    Final 100%

--- js/script.js
+++ js/script.js
@@ -5,3 +5,47 @@ $(document).ready(function () {
         slidesToScroll: 1
     });
 });
+
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('.pages-item').on('click', function () {
+        $('.pages-item').removeClass('active');
+        $(this).addClass('active');
+        
+        if($('.pages-item-ideas').hasClass("active")){
+            $('.pages-descr').fadeOut();
+            $('.pages-descr-ideas').fadeIn();
+        }
+        
+        if($('.pages-item-solution').hasClass("active")){
+            $('.pages-descr').fadeOut();
+            $('.pages-descr-solution').fadeIn();
+        }
+        
+        if($('.pages-item-touch').hasClass("active")){
+            $('.pages-descr').fadeOut();
+            $('.pages-descr-touch').fadeIn();
+        }
+        
+        if($('.pages-item-winning').hasClass("active")){
+            $('.pages-descr').fadeOut();
+            $('.pages-descr-winning').fadeIn();
+        }
+    })
+})
+

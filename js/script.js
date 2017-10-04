156ec7a31b1354e4fa06e7968d157b5c8b4dd021
AlLosevK <al.losev.k@gmail.com>
2017-10-04 21:21:34 +0300

    99%

--- js/script.js
+++ js/script.js
@@ -15,6 +15,22 @@ $(document).ready(function () {
 });
 
 $(function () {
+    var CountUpFlag = 0;
+    var $window = $(window);
+    $window.on('load scroll', function () {
+        if ($(this).scrollTop() >= 509 && CountUpFlag == 0) {
+            CountUp();
+            CountUpFlag = 1;
+        }
+    });
+    function CountUp() {
+        $('#top').show();
+    }
+});
+
+/* Smooth Scroll */
+
+$(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -22,38 +38,37 @@ $(function () {
             if (target.length) {
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000);
+                }, 1200);
+                $('#top').hide();
                 return false;
             }
         }
     });
 });
 
-
 $(document).ready(function () {
     $('.pages-item').on('click', function () {
         $('.pages-item').removeClass('active');
         $(this).addClass('active');
-        
-        if($('.pages-item-ideas').hasClass("active")){
+
+        if ($('.pages-item-ideas').hasClass("active")) {
             $('.pages-descr').fadeOut(0);
             $('.pages-descr-ideas').fadeIn(10);
         }
-        
-        if($('.pages-item-solution').hasClass("active")){
+
+        if ($('.pages-item-solution').hasClass("active")) {
             $('.pages-descr').fadeOut(0);
             $('.pages-descr-solution').fadeIn();
         }
-        
-        if($('.pages-item-touch').hasClass("active")){
+
+        if ($('.pages-item-touch').hasClass("active")) {
             $('.pages-descr').fadeOut(0);
             $('.pages-descr-touch').fadeIn();
         }
-        
-        if($('.pages-item-winning').hasClass("active")){
+
+        if ($('.pages-item-winning').hasClass("active")) {
             $('.pages-descr').fadeOut(0);
             $('.pages-descr-winning').fadeIn();
         }
     })
 })
-
